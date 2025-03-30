b15297b6f03bb14626c292da493fc8742a589668
rycejrvy <goat.rj23@gmail.com>
2025-03-30 13:44:27 -1000

    First Attempt
    DNF

---
+++ src/components/ContactCard.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+/* eslint-disable import/extensions */
+
+import { Card, Image } from 'react-bootstrap';
+import { Contact } from '@/lib/validationSchemas';
+
+const ContactCard = ({ contact }: { contact: Contact }) => (
+  <Card className="h-100">
+    <Card.Header>
+      <Image src={contact.image} width={75} />
+      <Card.Title>
+        {contact.firstName}
+        &nbsp;
+        {contact.lastName}
+      </Card.Title>
+      <Card.Subtitle>{contact.address}</Card.Subtitle>
+    </Card.Header>
+    <Card.Body>
+      <Card.Text>{contact.description}</Card.Text>
+    </Card.Body>
+  </Card>
+);
+
+export default ContactCard;
