8d191be3efabbc3c51596dae441492b2caeb7e59
rycejrvy <goat.rj23@gmail.com>
2025-04-05 14:54:47 -1000

    Second Attempt
    notes-2 finished in 27 minutes.

--- src/components/ContactCardAdmin.tsx
+++ src/components/ContactCardAdmin.tsx
@@ -4,10 +4,12 @@
 
 import Card from 'react-bootstrap/Card';
 import Image from 'react-bootstrap/Image';
-import { Contact } from '@/lib/validationSchemas';
+import { Contact, Note } from '@prisma/client';
+import ListGroup from 'react-bootstrap/ListGroup';
+import NoteItem from './NoteItem';
 
 /* Renders a single row in the List Stuff table. See list/page.tsx. */
-const ContactCardAdmin = ({ contact }: { contact: Contact }) => (
+const ContactCardAdmin = ({ contact, notes }: { contact: Contact; notes: Note[] }) => (
   <Card className="h-100">
     <Card.Header>
       <Image src={contact.image} width={75} />
@@ -24,6 +26,9 @@ const ContactCardAdmin = ({ contact }: { contact: Contact }) => (
       <Card.Text>
         {contact.description}
       </Card.Text>
+      <ListGroup variant="flush">
+        {notes.map((note) => <NoteItem key={note.id} note={note} />)}
+      </ListGroup>
     </Card.Body>
     <Card.Footer className="blockquote-footer">
       {contact.owner}
---
+++ src/components/NoteItem.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+/* eslint-disable import/extensions */
+
+import { Note } from '@prisma/client';
+import { ListGroup } from 'react-bootstrap';
+
+const NoteItem = ({ note }: { note: Note }) => (
+  <ListGroup.Item>
+    <p className="fw-lighter">{note.createdAt.toLocaleDateString('en-US')}</p>
+    <p>{note.note}</p>
+  </ListGroup.Item>
+);
+
+export default NoteItem;
